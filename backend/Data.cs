591c22f1b016f356035a7f8e1591f907ed1932af
Arash <arash.mahmoudhashemi@points.com>
2022-04-28 21:20:51 +0000

    fix diamond

--- backend/Data.cs
+++ backend/Data.cs
@@ -3,7 +3,7 @@ public static class Data {
         {"EAS1kfpgmLRo5WAZdJgpCzZtY6ak44pVsT3DxoeeQWfx", 
             new Response{Name = "Hilton Gold", Benefits = new List<string> {"Fifth night free", "Free bottled water" } }},
         {"3DtohyN3EQyBkie4zuyyprmCFZLDrvpohs1xxqyyh8GF", 
-            new Response{Name = "Hilton Gold", Benefits = new List<string> {"Premium WiFi", "Space-available room upgrades"} }},
+            new Response{Name = "Hilton Diamond", Benefits = new List<string> {"Premium WiFi", "Space-available room upgrades"} }},
         {"G5VzDcc7PoNMzz4oubCC2tppSPphjkmDeZXJFPv3vEWb", 
             new Response{Name = "Lyft", Benefits = new List<string> {"5% discount"} }}
     };
